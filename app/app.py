0cb1031b6ba3d17b688a8379222e84b3d14890ce
perryjsteward <perryjsteward@gmail.com>
2017-10-18 18:07:33 +0100

    small update

--- app/app.py
+++ app/app.py
@@ -1,13 +1,7 @@
 from flask import Flask
 import time
-from apscheduler.scheduler import Scheduler
 
 app = Flask(__name__)
-
-def test_scheduler():
-     print "TEST"
-     print time.time()
-
 
 # Can add more routes in here to create an API for querying data from influx
 @app.route("/")
